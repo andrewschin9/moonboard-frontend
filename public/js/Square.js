3c1133c0546aedb7b1b5a4a5a06b1a77dab5b339
Andrew Chin <andrewschin9@gmail.com>
2021-01-04 19:24:20 -0600

    add board grid

--- public/js/Square.js
+++
@@ -1,8 +0,0 @@
-class Square {
-  constructor() {
-    this.value = null;
-    this.isStart = false;
-    this.isMid = false;
-    this.isFinish = false;
-  }
-}
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,5 +1,65 @@
 <template>
-  <div class="board">
-    <div id="game-view">
+  <div class="home">
+    <table class="board">
+      <tr v-for="(row, index1) in holds">
+        <td v-for="(hold, index2) in row" v-on:click="changeTo(index1, index2)">
+          {{ hold }}
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
+<style>
+table,
+th,
+td {
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 50px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  border: 1px solid black;
+  border-collapse: collapse;
+  height: 40px;
+  width: 40px;
+}
+</style>
+<script>
+export default {
+  data: function () {
+    return {
+      holds: [
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+      ],
+    };
+  },
+  created: function () {},
+  methods: {
+    changeTo: function (index1, index2) {
+      console.log(index1, index2);
+      this.holds[index1][index2] = "O";
+      console.log(this.holds);
+      this.$forceUpdate();
+      // this.information = thing;
+    },
+  },
+};
+</script>
